f8f6b8fbc8ca5c4f248210ea309227665413070c
Yo Shium Wong <31518615+yoshiumw@users.noreply.github.com>
2023-08-24 15:22:39 -0600

    Update app.py with correct data syntax

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/test_data')
+@app.route('/test_data', methods=["GET"])
 def test_data():
-    data = "{ "totalCount": 1, "pageSize": 50, "problems": [ { "title": "Http monitor local outage", "affectedEntities": [ { "entityId": { "id": "HTTP_CHECK-A2313049293E", "type": "HTTP_CHECK" }, "name": "ENTITY1_HEALTH_STATUS" } ] }, { "title": "Http monitor local outage", "affectedEntities": [ { "entityId": { "id": "HTTP_CHECK-B23213131A", "type": "HTTP_CHECK" }, "name": "ENTITY2_HEALTH_STATUS" } ] } ] }"
-    return jsonify(data)
+    data = { "totalCount": 1, "pageSize": 50, "problems": [ { "title": "Http monitor local outage", "affectedEntities": [ { "entityId": { "id": "HTTP_CHECK-A2313049293E", "type": "HTTP_CHECK" }, "name": "ENTITY1_HEALTH_STATUS" } ] }, { "title": "Http monitor local outage", "affectedEntities": [ { "entityId": { "id": "HTTP_CHECK-B23213131A", "type": "HTTP_CHECK" }, "name": "ENTITY2_HEALTH_STATUS" } ] } ] }
+    return json.dumps(data)
