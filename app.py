10558a6f82ec1556682051d2b873817d2efda1ca
Yo Shium Wong <31518615+yoshiumw@users.noreply.github.com>
2023-08-24 15:15:45 -0600

    Update app.py with test data

--- app.py
+++ app.py
@@ -4,3 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/test_data')
+def test_data():
+    data = "{ "totalCount": 1, "pageSize": 50, "problems": [ { "title": "Http monitor local outage", "affectedEntities": [ { "entityId": { "id": "HTTP_CHECK-A2313049293E", "type": "HTTP_CHECK" }, "name": "ENTITY1_HEALTH_STATUS" } ] }, { "title": "Http monitor local outage", "affectedEntities": [ { "entityId": { "id": "HTTP_CHECK-B23213131A", "type": "HTTP_CHECK" }, "name": "ENTITY2_HEALTH_STATUS" } ] } ] }"
+    return jsonify(data)
